6e1d79007af44d6ec0092dc2db5172cad553a7a2
webdev <webdev1264@gmail.com>
2023-11-04 10:46:11 +0100

    Added BookForm, BookList and Filter components

---
+++ frontend/src/components/BookList/BookList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./BookList.css";
+
+const BookList = () => {
+  return (
+    <div className="app-block book-list">
+      <h2>Book List</h2>
+    </div>
+  );
+};
+
+export default BookList;
---
+++ frontend/src/components/Filter/FIlter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Filter.css";
+
+const FIlter = () => {
+  return (
+    <div className="app-block filter">
+      <h2>Filter</h2>
+    </div>
+  );
+};
+
+export default FIlter;
