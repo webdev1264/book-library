321bcbfa18ba1cc2b474b29a2b644608741e3397
webdev <webdev1264@gmail.com>
2023-11-04 09:53:50 +0100

    Initial React App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Book library App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
