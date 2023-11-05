f9b7c37c0b013740a7b5dc7322fb7e495898ab50
webdev <webdev1264@gmail.com>
2023-11-06 00:08:38 +0100

    Created API service

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const booksData = require("./data/books.json");
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+
+app.get("/random-book", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * booksData.length);
+  const randomBook = booksData[randomIndex];
+  res.json(randomBook);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
